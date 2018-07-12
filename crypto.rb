22a13af0b3ca85db53881477c1ce46759e495283
Anisoara <barbalatana@gmail.com>
2018-07-12 23:14:02 +0200

    Ajout du fichier crypto.rb

--- crypto.rb
+++ crypto.rb
@@ -1,24 +1,24 @@
 require 'rubygems'
 require 'nokogiri'   
 require 'open-uri'
-
-def names
-	arr = []
-	doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")).css(".currency-name-container").each do |names|
-		arr.push(names.text)
+while true
+	def names
+		arr = []
+		doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")).css(".currency-name-container").each do |names|
+			arr.push(names.text)
+		end
+		return arr
 	end
-	return arr
-end
-
-def price
-	arr = []
-	doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")).css(".price").each do |price|
-		arr.push(price.text)
+	def price
+		arr = []
+		doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")).css(".price").each do |price|
+			arr.push(price.text)
+		end
+		return arr
 	end
-	return arr
-end
-
-var = Hash[names.zip(price)]
-var.each do |names, price|
-	puts "#{names}: #{price}"
+	var = Hash[names.zip(price)]
+	var.each do |names, price|
+		puts "#{names}: #{price}"
+	end
+	sleep(3600)
 end
